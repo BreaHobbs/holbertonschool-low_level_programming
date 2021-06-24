b7d8f084cbc3d207cb06023c1f6c5b43a77617a6
Brea Hobbs <3236@holbertonschool.com>
2021-06-24 09:57:44 -0400

    added a space

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 fwrite("and that piece of art is useful\"", 32, 1, stderr);
-fwrite("- Dora Korpar, 2015-10-19\n", 27, 1, stderr);
+fwrite(" - Dora Korpar, 2015-10-19\n", 28, 1, stderr);
 return (0);
 }
