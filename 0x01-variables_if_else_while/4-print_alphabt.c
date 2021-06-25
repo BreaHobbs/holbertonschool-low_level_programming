43d23c9ece7d6c97b945172a40b47213103b87c2
Brea Hobbs <3236@holbertonschool.com>
2021-06-25 13:46:42 -0400

    prints alphabet minus 2 letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Prints the alphabet using putchar commands
+*
+* Return: returns successfully a 0 upon completion
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch == 'e' || ch == 'q')
+continue;
+{
+putchar (ch);
+
+continue;
+
+}
+putchar ('\n');
+return (0);
+}
+}
