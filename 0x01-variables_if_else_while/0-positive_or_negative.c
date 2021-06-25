7d317adbd647f85c35e9b0a7b2b42221b868f9e5
Brea Hobbs <3236@holbertonschool.com>
2021-06-25 10:03:13 -0400

    created directory 0x01, created file 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - This program will assign a random number to the variable n
+*
+* Return: returns successfully a 0 upon completion
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("%d", n);
+if (n == 0)
+{
+printf(" is zero\n");
+}
+else if (n > 0)
+{
+printf(" is positive\n");
+}
+else
+{
+printf(" is negative\n");
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Prints string to stderr using fwrite
+*
+* Return: returns successfully a 1 upon completion
+*/
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
