be6724610559639f74e1497787b51c13e959c1f5
Brea Hobbs <3236@holbertonschool.com>
2021-06-25 14:23:15 -0400

    prints 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Prints numbers 0-9 followed by new line
+*
+* Return: returns successfully a 0 upon completion
+*/
+int main(void)
+{
+int x;
+
+for (x = 0; x < 10; x++)
+{
+printf("%d", x);
+}
+printf("\n");
+}
