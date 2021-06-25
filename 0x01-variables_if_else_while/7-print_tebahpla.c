1575bb3465b74390a66bc7ed4e9b9e8596dd384c
Brea Hobbs <3236@holbertonschool.com>
2021-06-25 16:48:02 -0400

    Prints the alphabet backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Prints backwards alphabet using putchar commands
+*
+* Return: returns successfully a 0 upon completion
+*/
+int main(void)
+{
+int counter;
+for (counter = 25; counter >= 0 ; counter--)
+{
+putchar (65 + counter);
+}
+putchar ('\n');
+return (0);
+}
