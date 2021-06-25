39fd1d6c82e19b3a66199cfe8acfec653e4642f6
Brea Hobbs <3236@holbertonschool.com>
2021-06-25 10:46:48 -0400

    wrote a script that prints the last digit of number with tests using if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,9 +8,25 @@
 */
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+        int n, lastDigit;
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+	lastDigit = n % 10;
+        printf("Last digit of %d is ", n);
+        printf("%d ", lastDigit);
+	if (lastDigit > 5)
+	{
+	printf("and is greater than 5\n");
+	}
+	else if (lastDigit < 6 && lastDigit!=0 )
+	{
+	printf("and is less than 6 and not 0\n");
+	}
+	else 
+	{
+	printf("and is 0\n");
+	}
+	        
+return (0);
 }
