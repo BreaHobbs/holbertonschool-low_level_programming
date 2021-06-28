2afa7111b5956f7ba41ef2dbf957bfee874511a6
Brea Hobbs <3236@holbertonschool.com>
2021-06-28 10:20:14 -0400

    created directory and 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Write a program that prints Holberton, followed by a new line.
+*
+* Return: returns successfully a 0 upon completion
+*/
+int main(void)
+{
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Write a function that prints the alphabet, in lowercase.
+*
+* Return: returns successfully a 0 upon completion
+*/
+void print_alphabet(void);
+
+int main(void)
+{
+
+
+}
