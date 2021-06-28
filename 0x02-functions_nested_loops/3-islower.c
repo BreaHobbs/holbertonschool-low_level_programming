e2614eeaedf9ce337d6b7dce116cb6ebc9a175aa
Brea Hobbs <3236@holbertonschool.com>
2021-06-28 17:51:24 -0400

    create ranges

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,14 @@
 #include "holberton.h"
 /**
 * int _islower(int c); - function checks for lowercase character
-*
+* 
 * Return: returns successfully a 0 upon completion
 */
 int _islower(int c);
 {
-	if (c _islower)
+	int c;
+
+	if (c >= 65 && 122 <= a)
 	{
 	return (1);
 	}
