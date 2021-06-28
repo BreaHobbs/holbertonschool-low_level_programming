7cc46c2f61f4f1a6ac7a8eda3ffbb5d8e78739c1
Brea Hobbs <3236@holbertonschool.com>
2021-06-28 19:24:50 -0400

    created 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+* print_sign - function checks for lowercase character
+* @n: takes in input into function
+* Return: returns successfully a 0 upon completion
+*/
+int print_sign(int n)
+{
+        if (n >= 0)
+        {
+	printf("+");	
+        return (1);
+        }
+     	else if (n = 0)
+	printf("0");	
+        return (0);
+	else
+	printf("-");
+        return (-1);
+}
