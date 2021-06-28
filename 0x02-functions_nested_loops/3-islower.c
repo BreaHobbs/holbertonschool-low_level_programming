0d4abf03607dc75d8e9a8509add8664e45234640
Brea Hobbs <3236@holbertonschool.com>
2021-06-28 17:39:16 -0400

    created 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+* int _islower(int c); - function checks for lowercase character
+*
+* Return: returns successfully a 0 upon completion
+*/
+int _islower(int c);
+{
+	if (c _islower)
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
