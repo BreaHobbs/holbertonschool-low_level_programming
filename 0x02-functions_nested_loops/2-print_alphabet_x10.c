13f5f38ffe4446281b8e9f57505e7730b77843bc
Brea Hobbs <3236@holbertonschool.com>
2021-06-28 17:23:11 -0400

    alphabet 10x hopefully

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
-* main - Write a program that prints Holberton, followed by a new line.
+* print_alphabet_x10 - Prints the alphabet 10 times
 *
 * Return: returns successfully a 0 upon completion
 */
@@ -11,12 +11,11 @@ void print_alphabet_x10(void)
 	char c;
 	int x;
 
-	for (x = 0 ; x <= 10 ; x++)
+	for (x = 0 ; x < 10 ; x++)
 	{
 	for (c = 'a' ; c <= 'z' ; c++)
 	{
-		 _putchar (c);
-
+		_putchar (c);
 	}
 		_putchar ('\n');
 	}
