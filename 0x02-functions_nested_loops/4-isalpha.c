b79dd140d1aaadc5d5c649b84801dc5326c2dc49
Brea Hobbs <3236@holbertonschool.com>
2021-06-28 18:57:02 -0400

    created prototype

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+* _isalpha - function checks for alphabetic character
+* @c: takes in input into function
+* Return: returns successfully a 0 upon completion
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
