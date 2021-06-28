efb53d0f4003ce62ae8ced988e3a5e7b1b84b572
Brea Hobbs <3236@holbertonschool.com>
2021-06-28 19:29:32 -0400

    added printed lines and formatting

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,19 @@
 */
 int print_sign(int n)
 {
-        if (n >= 0)
-        {
-	printf("+");	
-        return (1);
-        }
-     	else if (n = 0)
-	printf("0");	
-        return (0);
+	if (n >= 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
 	else
-	printf("-");
-        return (-1);
+	{
+		printf("-");
+		return (-1);
+	}
 }
