c8ea5708ce45b868c99d3e2e368faccd09fa40bf
Brea Hobbs <3236@holbertonschool.com>
2021-06-29 14:36:40 -0400

    created 11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* print_to_98 - function checks for lowercase character
+* @n: takes in input into function
+* Return: returns successfully a 0 upon completion
+*/
+void print_to_98(int n)
+{
+	for (n = 0; n > 1; n++)
+	{
+	printf("%d, ", n);
+	}
+
+
+}
