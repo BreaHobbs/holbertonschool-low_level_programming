03cd7309636bd225f3d486c316b7192951c47499
Brea Hobbs <3236@holbertonschool.com>
2021-06-28 23:16:11 -0400

    created script that adds values together

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* add - function that adds two results together
+* @x: takes in input into function
+* @y: takes in other input into function
+* Return: returns successfully a 0 upon completion
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
