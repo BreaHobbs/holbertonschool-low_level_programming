4c5cb40415972a5303b9a4cd6172775464c9a471
Brea Hobbs <3236@holbertonschool.com>
2021-06-28 23:37:14 -0400

    wrote a script that displays all minutes in 24 hour day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+* jack_bauer - function checks for lowercase character
+* @x: takes in input into function
+* Return: returns successfully a 0 upon completion
+*/
+int jack_bauer(void)
+{
+	int hours = 0;
+	
+	while (hours < 24)
+	{
+		int minutes = 0;
+		while (minutes < 60)
+		{
+			if (hours < 10)
+			{
+				_putchar ('0');
+				
+			}	
+			_putchar (hours);
+			_putchar (':');
+			if (minutes < 10)
+			{
+				_putchar('0');
+				
+			}
+			_putchar(minutes);
+			minutes++;
+			_putchar('\n');
+		}	
+		hours++;
+	}
+}
+
