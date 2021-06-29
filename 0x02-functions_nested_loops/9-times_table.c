631cc252b1b8f31c8faad1dffcc6b8424555bc11
Brea Hobbs <3236@holbertonschool.com>
2021-06-29 18:20:31 -0400

    created 9-timestable.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* _times_table -  a function that prints the 9 times table, starting with 0.
+* @c: takes in input into function
+* Return: returns successfully a 0 upon completion
+*/
+void times_table(void)
+{
+	int c;
+
+	for (c = 0; c <= 10; c++)
+	{
+	_putchar(c);
+	}
+
+}
