367ab8936d6b19a9e4588bbde8c620afa9f01a5f
Brea Hobbs <3236@holbertonschool.com>
2021-06-28 22:25:11 -0400

    typo

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* print_last_digit - function checks for lowercase character
+* @x: takes in input into function
+* Return: returns successfully a 0 upon completion
+*/
+int print_last_digit(int x)
+{
+	int num;
+
+	x = num % 10;
+
+return (x);
+}
