57408325f2535354b5f71d81dc117a4adc12107f
Brea Hobbs <3236@holbertonschool.com>
2021-07-02 13:44:32 -0400

    made changes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,16 @@ void more_numbers(void)
 	int y;
 
 	for (y = 0; y <= 10; y++)
-{
-	for (x = 0; x <= 14; x++)
+	{
+		for (x = 0; x <= 14; x++)
 	{
 		if (x > 9)
-
-			_putchar((x / 10) + '0');
-			_putchar((x % 10) + '0');
-}
+			
+				
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+			
+	}
 
 	_putchar('\n');
 }
