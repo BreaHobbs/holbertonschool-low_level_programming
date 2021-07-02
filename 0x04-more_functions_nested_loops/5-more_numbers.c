92eeb7f65c5de4b526271709c425c38fc71d1abd
Brea Hobbs <3236@holbertonschool.com>
2021-07-02 13:58:00 -0400

    betty linter

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,15 +12,12 @@ void more_numbers(void)
 	{
 		for (x = 0; x <= 14; x++)
 	{
-		if (x > 9)	
+		if (x > 9)
 		{
 			_putchar((x / 10) + '0');
 		}
-		
 		_putchar((x % 10) + '0');
-			
 	}
-
 	_putchar('\n');
 }
 }
