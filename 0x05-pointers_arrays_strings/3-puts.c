225eb79975a53b559cd9dae158dcda3bc02edee3
Brea Hobbs <3236@holbertonschool.com>
2021-07-04 12:24:51 -0400

    betty styling

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
-* print_last_digit - function checks for lowercase character
-* @x: takes in input into function
+* _puts - function checks for lowercase character
+* @str: takes in input into function
 * Return: returns successfully a 0 upon completion
 */
 void _puts(char *str)
 {
 char c = '\0';
-  while((c = *str++))
-    _putchar(c);
-  _putchar('\n');
+
+while ((c = *str++))
+	_putchar(c);
+	_putchar('\n');
 
 
 }
