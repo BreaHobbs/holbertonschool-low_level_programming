ca42d7eb682048f29724fb7b1803009a46a18167
Brea Hobbs <3236@holbertonschool.com>
2021-07-04 12:19:28 -0400

    betty styling

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
 * _strlen - Write a function that returns the length of a string.
-* @count: takes in input into function
+* @p: takes input into function
 * Return: returns successfully a 0 upon completion
 */
 int _strlen(char *p)
 {
-    unsigned int count = 0;
+	unsigned int count = 0;
 
-    while(*p!='\0')
-    {
-        count++;
-        p++;
-    }
-    return count;
+	while (*p != '\0')
+	{
+		count++;
+		p++;
+	}
+	return (count);
 }
