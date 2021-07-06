dd1ce25bead00a2b4b0deef51a154ec8b5a4f3cd
Brea Hobbs <3236@holbertonschool.com>
2021-07-06 10:41:19 -0400

    betty stylin

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
 * puts2 - Write a function that returns the length of a string.
-* @p: takes input into function
+* @str: takes input into function
 * Return: returns successfully a 0 upon completion
 */
 void puts2(char *str)
 {
 	int i = 0;
 
-for(i = 0; i < _strlen(str); i += 2)
+	for (i = 0; i < _strlen(str); i += 2)
 {
-   _putchar(str[i]);
+	_putchar(str[i]);
 }
 _putchar('\n');
 }
@@ -21,12 +21,12 @@ _putchar('\n');
 */
 int _strlen(char *p)
 {
-        unsigned int count = 0;
+	unsigned int count = 0;
 
-        while (*p != '\0')
-        {
-                count++;
-                p++;
-        }
-        return (count);
+	while (*p != '\0')
+	{
+		count++;
+		p++;
+	}
+	return (count);
 }
