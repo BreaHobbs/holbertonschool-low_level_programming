4e7c8749bed21fe4327a304dcc6155f4f8b5c952
Brea Hobbs <3236@holbertonschool.com>
2021-07-07 09:47:08 -0400

    created 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* puts_half - function prints half of a string
+* @str: takes input into function
+* Return: returns successfully a 0 upon completion
+*/
+void puts_half(char *str)
+{
+	int n;
+
+	int counter;
+	
+	int length_of_the_string = _strlen(str); //gets length of string//
+
+	if (length_of_the_string % 2 == 0) //is even//
+	{	
+		n = (length_of_the_string / 2);
+		for (counter = n; counter < length_of_the_string; counter++)
+		{
+			
+			_putchar(str[counter]);
+			
+		}	
+		
+	}
+	else if (length_of_the_string - 1 / 2 != 0) //is odd//
+	{
+		n = (length_of_the_string -1 / 2);
+		for (counter = n; counter < length_of_the_string; counter++)
+			_putchar(str[counter]);	
+	}	
+		_putchar('\n');
+}
+
+/**
+* _strlen - Write a function that returns the length of a string.
+* @p: takes input into function
+* Return: returns successfully a 0 upon completion
+*/
+int _strlen(char *p)
+{
+        unsigned int count = 0;
+
+        while (*p != '\0')
+        {
+                count++;
+                p++;
+        }
+        return (count);
+}
