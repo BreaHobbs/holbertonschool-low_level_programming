933d615eb22b2386a185864b907ad2c196d5b46c
Brea Hobbs <3236@holbertonschool.com>
2021-07-07 10:49:16 -0400

    betty sylin

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 /**
 * _strcpy - function prints half of a string
-* @str: takes input into function
+* @src: takes input into function
+* @dest: also takes input into function
 * Return: returns successfully a 0 upon completion
 */
 char *_strcpy(char *dest, char *src)
 {
 
-	char * temp = dest;
-	while ((*temp++ = *src++));
-	return (dest);
+	char  *temp = dest;
 
+	while ((*temp++ = *src++))
+
+	return (dest);
 }
