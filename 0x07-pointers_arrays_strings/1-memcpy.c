c824ea86d4c1a64e8045fddae40d66f2e917f31e
Brea Hobbs <3236@holbertonschool.com>
2021-07-09 16:20:13 -0400

    betty styling

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,19 +6,19 @@
  * *_memcpy - Write a function that copies memory area.
  * @dest: a string
  * @src: a string
- * @n - variable takes in input
+ * @n: variable takes in input
  * Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-   // Typecast src and dest addresses to (char *)
-   char *csrc = (char *)src;
-   char *cdest = (char *)dest;
-  
-   // Copy contents of src[] to dest[]
-   for ( i=0; i<n; i++)
-       cdest[i] = csrc[i];
+	/* Typecast src and dest addresses to (char *)*/
+	char *csrc = (char *)src;
+	char *cdest = (char *)dest;
+
+	/* Copy contents of src[] to dest[]*/
+	for (i = 0; i < n; i++)
+		cdest[i] = csrc[i];
 
 return (dest);
 
