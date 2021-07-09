d4ef22a06006ca5d8a6ba31299f5237308311faa
Brea Hobbs <3236@holbertonschool.com>
2021-07-08 22:47:21 -0400

    created file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * *cap_string - capitalizes first letter of every word
+ * @str: takes in input
+ * Return: dest
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		/*check first character is lowercase alphabet*/
+		if (i == 0)
+		{
+			if ((str[i] >= 'a' && str[i] <= 'z'))
+				str[i] = str[i] - 32; /*subtract 32 to make it capital*/
+			continue; /*continue to the loop*/
+		}
+		if (str[i] == ' ')/*check space*/
+		{
+			/*if space is found, check next character*/
+			++i;
+			/*check next character is lowercase alphabet*/
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32; /*subtract 32 to make it capital*/
+				continue; /*continue to the loop*/
+			}
+		}
+		else
+		{
+			/*all other uppercase characters should be in lowercase*/
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32; /*subtract 32 to make it small/lowercase*/
+		}
+	}
+return (str);
+}
