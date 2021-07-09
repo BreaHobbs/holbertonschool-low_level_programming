60b26007fc5cd52f3c0f164b30c43a995eb479c8
Brea Hobbs <3236@holbertonschool.com>
2021-07-08 23:15:51 -0400

    added separator function

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+/**
+ * seperator_comp - checks input for seperator character
+ * @n: value
+ *
+ * Return: whether it is or not
+ */
+int is_separator(int n)
+{
+	int answer = 0, i;
+	char is_seperator[] = "' ''\t''\n',;.!?\"(){}";
+
+	for (i = 0; is_seperator[i] != '\0'; i++)
+		if (n == is_seperator[i])
+			answer = 1;
+	return (answer);
+}
 /**
  * *cap_string - capitalizes first letter of every word
  * @str: takes in input
@@ -20,11 +37,14 @@ char *cap_string(char *str)
 				str[i] = str[i] - 32; /*subtract 32 to make it capital*/
 			continue; /*continue to the loop*/
 		}
-		if (str[i] == ' ')/*check space*/
+		if (is_separator(str[i]) == 1)
 		{
 			/*if space is found, check next character*/
-			++i;
+			i++;
 			/*check next character is lowercase alphabet*/
+			if (is_separator(str[i]) == 1)
+				i++;
+			
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] = str[i] - 32; /*subtract 32 to make it capital*/
